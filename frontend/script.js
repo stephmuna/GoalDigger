f34e8d55dc07b1854f57e3ed98f74d236d1b42ed
Yasmin Kabir <yasminkabirofficial@gmail.com>
2025-04-05 18:03:45 -0700

    script for java

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+document.getElementById("signup-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    const formData = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value
+    };
+  
+    fetch("http://127.0.0.1:8080", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("Response from backend:", data);
+        alert("Signed up successfully!");
+      })
+      .catch(err => {
+        console.error("Error:", err);
+      });
+  });
+  
